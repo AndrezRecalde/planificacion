f6373e61e2d64badf1f17d80f1d919ae11230317
AndrezRecalde <azw1021@gmail.com>
2024-02-05 01:08:21 -0500

    se agrega autorization

--- resources/js/src/api/planningApi.js
+++ resources/js/src/api/planningApi.js
@@ -4,15 +4,15 @@ import { getEnv } from "../helpers/getEnv";
 const { VITE_APP_URL } = getEnv();
 
 const planningApi = axios.create({
-    baseURL: VITE_APP_URL
+    baseURL: VITE_APP_URL,
 });
 
-planningApi.interceptors.request.use(config => {
+planningApi.interceptors.request.use((config) => {
     config.headers = {
         ...config.headers,
-        "Accept": "application/json",
-
-    }
+        Accept: "application/json",
+        Authorization: "Bearer " + localStorage.getItem("auth_token"),
+    };
     return config;
 });
 
