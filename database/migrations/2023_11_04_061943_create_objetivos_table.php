16fc0fbab4f0d43781b7e5b4373abe89ebd2c652
AndrezRecalde <azw1021@gmail.com>
2024-07-16 00:42:01 -0500

    se modifica tabla de objetivos

--- database/migrations/2023_11_04_061943_create_objetivos_table.php
+++ database/migrations/2023_11_04_061943_create_objetivos_table.php
@@ -9,22 +9,24 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+    /* Objetivos Estrategicos del PDOT */
     public function up(): void
     {
         Schema::create('objetivos', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre_objetivo');
             $table->unsignedBigInteger('lestrategiapdot_id');
             $table->unsignedBigInteger('competenciapdot_id');
             //$table->unsignedBigInteger('componentepdot_id');
-            $table->unsignedBigInteger('gestioncumplimiento_id'); //GESTION RESPONSABLE DEL CUMPLIMIENTO: FORANEA DE DEPARTAMENTOS
-            $table->unsignedBigInteger('oepdot_id');
+            $table->unsignedBigInteger('gestioncumplimiento_id'); //GESTION RESPONSABLE DEL CUMPLIMIENTO: FORANEA DE DEPARTAMENTOS //TODO: HACERLO N:M
+            /* $table->unsignedBigInteger('oepdot_id'); */
             $table->unsignedBigInteger('earticulacion_id');
             $table->unsignedBigInteger('metapdot_id');
             $table->string('indicadorpdot');
             $table->unsignedBigInteger('competencia_id'); // COMPETENCIAS DEL GAD
             $table->unsignedBigInteger('rmedicion_id'); // GESTION RESPONSABLE DE MEDICION: FORÁNEA DE DEPARTAMENTOS
             /* $table->unsignedBigInteger('gestionresponsable_id'); */ // GESTION RESPONSABLE DEL CUMPLIMIENTO OPERATIVO: FORÁNEA DE DEPARTAMENTOS
-            $table->unsignedBigInteger('oepei_id')->nullable();
+            /* $table->unsignedBigInteger('oepei_id')->nullable(); */
             $table->year('anio_cumplimiento');
             $table->string('linea_base');
             $table->year('anio_lbase');
@@ -36,13 +38,13 @@ return new class extends Migration
             $table->foreign('competenciapdot_id')->references('id')->on('competenciapdots')->onDelete('cascade');
             $table->foreign('componentepdot_id')->references('id')->on('componentepdots')->onDelete('cascade');
             $table->foreign('gestioncumplimiento_id')->references('id')->on('departamentos')->onDelete('cascade');
-            $table->foreign('oepdot_id')->references('id')->on('oepdots')->onDelete('cascade');
+            /* $table->foreign('oepdot_id')->references('id')->on('oepdots')->onDelete('cascade'); */
             $table->foreign('earticulacion_id')->references('id')->on('earticulaciones')->onDelete('cascade');
             $table->foreign('metapdot_id')->references('id')->on('metapdots')->onDelete('cascade');
             $table->foreign('competencia_id')->references('id')->on('competencias')->onDelete('cascade');
             $table->foreign('rmedicion_id')->references('id')->on('departamentos')->onDelete('cascade');
             /* $table->foreign('gestionresponsable_id')->references('id')->on('departamentos')->onDelete('cascade'); */
-            $table->foreign('oepei_id')->references('id')->on('oepeis')->onDelete('set null');
+            /* $table->foreign('oepei_id')->references('id')->on('oepeis')->onDelete('set null'); */
 
 
             // Añadir índices para mejorar el rendimiento de consultas
@@ -50,13 +52,13 @@ return new class extends Migration
             $table->index('competenciapdot_id');
             $table->index('componentepdot_id');
             $table->index('gestioncumplimiento_id');
-            $table->index('oepdot_id');
+            /* $table->index('oepdot_id'); */
             $table->index('earticulacion_id');
             $table->index('metapdot_id');
             $table->index('competencia_id');
             $table->index('rmedicion_id');
             /* $table->index('gestionresponsable_id'); */
-            $table->index('oepei_id');
+            /* $table->index('oepei_id'); */
         });
     }
 
